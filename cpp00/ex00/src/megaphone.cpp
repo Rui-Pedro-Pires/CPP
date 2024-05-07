148307bbedcafa3039da534364cfa2c5e2a4c245
Rui-Pedro-Pires <ruiolive@student.42porto.com>
2024-05-07 15:32:49 +0100

    Update megaphone.cpp to remove newline at end of file

--- cpp00/ex00/src/megaphone.cpp
+++ cpp00/ex00/src/megaphone.cpp
@@ -13,10 +13,11 @@ int main(int argc, char **argv)
     std::string temp;
 
     if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     for (int i = 1; i < argc; i++) {
         temp = (std::string)argv[i];
         ft_toupper(temp);
-        std::cout << temp << std::endl;
+        std::cout << temp;
     }
+    std::cout << std::endl;
 }
