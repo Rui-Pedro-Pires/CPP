8455e360075b5b7000d7041870a544abf609882e
ruiines <ruiines22@gmail.com>
2024-05-08 10:37:04 +0100

    Refactor Zombie class and main.cpp for consistent formatting

--- cpp01/ex00/includes/Zombie.hpp
+++ cpp01/ex00/includes/Zombie.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruiolive  <ruiolive@student.42.fr   >      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 10:27:00 by ruiolive          #+#    #+#             */
+/*   Updated: 2024/05/08 10:31:50 by ruiolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 #ifndef ZOMBIE_HPP
 #define ZOMBIE_HPP
@@ -6,13 +18,15 @@
 
 class Zombie
 {
-private:
-    std::string _name;
+
 public:
     Zombie( void );
     Zombie( std::string name );
     ~Zombie();
     void    announce( void );
+
+private:
+    std::string _name;
 };
 
 void    randomChump( std::string name );
