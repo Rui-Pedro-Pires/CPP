566cd7423295754953a5b4341b97ccd3803228af
Rui-Pedro-Pires <ruiolive@student.42porto.com>
2024-09-30 15:07:46 +0100

    Add Makefile and Bureaucrat class implementation

---
+++ cpp05/ex00/srcs/Bureaucrat.cpp
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Burecrat.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruiolive  <ruiolive@student.42.fr   >      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 13:45:22 by ruiolive          #+#    #+#             */
+/*   Updated: 2024/09/26 13:45:22 by ruiolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() {}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name)
+{
+    if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+    else if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    this->_grade = grade;
+    std::cout << "Bureaucrat " << this->_name << " created!" << std::endl;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Bureaucrat " << this->_name << " Destroyed!" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other)
+{
+    *this = other;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+    if (this != &other)
+    {
+        this->_name = other._name;
+        this->_grade = other._grade;
+    }
+    return *this;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade too high!";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade too low!";
+}
+
+std::string Bureaucrat::getname() const
+{
+    return this->_name;
+}
+
+int Bureaucrat::getgrade() const
+{
+    return this->_grade;
+}
+
+void Bureaucrat::incrementgrade()
+{
+    if (this->_grade == 1)
+        throw Bureaucrat::GradeTooHighException();
+    this->_grade--;
+}
+
+void Bureaucrat::decrementgrade()
+{
+    if (this->_grade == 150)
+        throw Bureaucrat::GradeTooLowException();
+    this->_grade++;
+}
+
+std::ostream &operator<<(std::ostream &stream, const Bureaucrat &Bureaucrat)
+{
+    stream << Bureaucrat.getname() << ">, bureaucrat grade " << Bureaucrat.getgrade();
+    return stream;
+}
