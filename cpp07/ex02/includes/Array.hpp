fafa3ef8c993249fa336df4cceb552938e6d696d
ruiolive <ruipedro.pires999@gmail.com>
2024-10-06 11:08:45 +0100

    Add Makefile for ex02

---
+++ cpp07/ex02/includes/Array.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   template.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruiolive  <ruiolive@student.42.fr   >      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 16:11:48 by ruiolive          #+#    #+#             */
+/*   Updated: 2024/10/04 16:11:48 by ruiolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+private:
+    T array;
+public:
+    Array();
+    Array(unsigned int N);
+    ~Array();
+    Array(const Array &other);
+    Array& operator=(const Array &other);
+    T* operator[]();
+    int size();
+};
+
+#endif
