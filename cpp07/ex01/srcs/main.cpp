94c59ce48300dd62ca4e3f2d66c4b0318970b58d
Rui-Pedro-Pires <ruiolive@student.42porto.com>
2024-10-08 09:59:12 +0100

    Refactor code in main.cpp

--- cpp07/ex01/srcs/main.cpp
+++ cpp07/ex01/srcs/main.cpp
@@ -12,13 +12,16 @@
 
 #include "../includes/iter.hpp"
 
-int main( void )
+int main(void)
 {
-    int array[] = { 1, 2, 3, 4, 5 };
-    for ( int i = 0; i < 5; ++i ) std::cout << array[i] << std::endl;
+    int array[] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << array[i] << std::endl;
+    }
     std::cout << "------------" << std::endl;
-    iter( array, 5, function<int> );
-    for ( int i = 0; i < 5; ++i )
+    iter(array, 5, function<int>);
+    for (int i = 0; i < 5; ++i)
     {
         std::cout << array[i] << std::endl;
     }
